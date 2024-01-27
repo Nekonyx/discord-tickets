7c6d5c37ec2e97dc67254ff196114e3da064835a
Nekonyx <nekonyx@proton.me>
2024-01-28 00:54:36 +0600

    refactor: ticket member add command

--- src/bot/commands/ticket/ticket-member.command.ts
+++ src/bot/commands/ticket/ticket-member.command.ts
@@ -1,24 +1,213 @@
 import {
   ApplicationCommandOptionType,
-  PermissionFlagsBits,
   CommandInteraction,
-  ThreadChannel,
   EmbedBuilder,
-  userMention,
-  roleMention,
-  TextChannel,
   GuildMember,
-  Collection,
+  PermissionFlagsBits,
   Role,
-  User
+  TextChannel,
+  ThreadChannel,
+  User,
+  roleMention,
+  userMention
 } from 'discord.js'
-import { SlashOption, SlashGroup, Discord, Slash } from 'discordx'
+import { Discord, Slash, SlashGroup, SlashOption } from 'discordx'
 
+import { Color } from '../../../constants'
 import { TicketService } from '../../../services/ticket.service'
 import { rootGroupName } from './constants'
-import { Color } from '../../../constants'
 
 const groupName = 'member'
+
+@SlashGroup(groupName, rootGroupName)
+@SlashGroup({
+  description: 'Управление участниками тикета',
+  root: rootGroupName,
+  name: groupName
+})
+@Discord()
+export class TicketMemberCommand {
+  private readonly ticketService = new TicketService()
+
+  @Slash({
+    description: 'Добавить участника или роль в тикет',
+    name: 'add'
+  })
+  public async add(
+    @SlashOption({
+      type: ApplicationCommandOptionType.Mentionable,
+      description: 'Кого добавить',
+      name: 'target',
+      required: true
+    })
+    target: GuildMember | Role,
+    interaction: CommandInteraction
+  ) {
+    await interaction.deferReply({
+      ephemeral: true
+    })
+
+    const ticket = await this.ticketService.getOne({
+      channelId: interaction.channelId
+    })
+
+    if (!ticket) {
+      return interaction.followUp({
+        content: 'Используйте эту команду в канале, который является тикетом'
+      })
+    }
+
+    /** Затронутые участники */
+    const members = new Set<GuildMember>()
+
+    /** Ветка с тикетом */
+    const thread = interaction.channel as ThreadChannel
+    /** Канал ветки */
+    const channel = thread.parent as TextChannel
+
+    // Если указан участник, то добавляем его
+    // Если указана роль, то добавляем всех участников с этой ролью
+    if (target instanceof GuildMember) {
+      members.add(target)
+    } else {
+      const guildMembers = await interaction.guild!.members.fetch()
+      const roleMembers = guildMembers.filter((m) => !m.user.bot && m.roles.cache.has(target.id))
+
+      // Защита от дурака
+      if (roleMembers.size >= 50) {
+        return interaction.followUp({
+          content: `Нельзя добавить больше 50 участников за раз`
+        })
+      }
+
+      for (const member of roleMembers.values()) {
+        members.add(member)
+      }
+    }
+
+    // Удаляем из списка участников, которые уже есть в тикете
+    for (const member of members) {
+      if (thread.members.cache.has(member.id)) {
+        members.delete(member)
+      }
+    }
+
+    // Если никого не осталось, то делать тут нечего
+    if (members.size === 0) {
+      return interaction.followUp({
+        content: `Все указанные участники уже есть в тикете`
+      })
+    }
+
+    const membersArray = [...members]
+
+    // Выдача прав на просмотр канала
+    const membersWithoutPermission = membersArray.filter((member) => {
+      return !channel.permissionsFor(member).has(PermissionFlagsBits.ViewChannel)
+    })
+
+    if (membersWithoutPermission.length > 0) {
+      await Promise.all(
+        membersWithoutPermission.map((member) =>
+          channel.permissionOverwrites.create(member, {
+            ViewChannel: true
+          })
+        )
+      )
+    }
+
+    // что-то на англосакском
+    // mentions work as members.add, but don't trigger non-deletable system message
+    const pingMessage = await thread.send(membersArray.map((m) => m.toString()).join())
+
+    await Promise.all([
+      pingMessage.delete(),
+      interaction.deleteReply(),
+      thread.send({
+        embeds: [
+          buildEmbed({
+            roleMembersMentions: membersArray.map((m) => m.toString()),
+            moderator: interaction.user,
+            target
+          })
+        ]
+      })
+    ])
+  }
+
+  @Slash({
+    description: 'Удалить участника или роль из тикета',
+    name: 'remove'
+  })
+  public async remove(
+    @SlashOption({
+      type: ApplicationCommandOptionType.Mentionable,
+      description: 'Кого удалить',
+      required: true,
+      name: 'target'
+    })
+    target: GuildMember | Role,
+    interaction: CommandInteraction
+  ) {
+    await interaction.deferReply({
+      ephemeral: true
+    })
+
+    const ticket = await this.ticketService.getOne({
+      channelId: interaction.channelId
+    })
+    const targetIsMember = target instanceof GuildMember
+
+    if (!ticket) {
+      return await interaction.followUp({
+        content: 'Используйте эту команду в канале, который является тикетом'
+      })
+    }
+
+    const channel = interaction.channel as ThreadChannel
+    // possible feature: delete categoryChannel permission overwrites if
+    // a) there were any b) it was the last user's ticket in the category
+
+    if (targetIsMember && !(await channel.members.fetch(target.id).catch(() => null))) {
+      return await interaction.followUp({
+        content: `${target} нет в тикете`
+      })
+    }
+
+    let roleMembers: GuildMember[] = []
+
+    if (!targetIsMember) {
+      roleMembers = (await channel.members.fetch())
+        .map((m) => m.guildMember as GuildMember)
+        .filter((m) => !m.user.bot && m.roles.resolve(target.id))
+
+      if (!roleMembers.length) {
+        return await interaction.followUp({
+          content: `В тикете не было участников с ролью ${target} (боты не учитываются)`
+        })
+      }
+    }
+
+    if (targetIsMember) {
+      await channel.members.remove(target.id) // also creates non-deletable system message
+    } else {
+      for (const member of roleMembers.values()) {
+        await channel.members.remove(member.id) // also creates non-deletable system message
+      }
+    }
+    interaction.deleteReply()
+    channel.send({
+      embeds: [
+        buildEmbed({
+          roleMembersMentions: roleMembers.map((m) => userMention(m.id)),
+          moderator: interaction.user,
+          add: false,
+          target
+        })
+      ]
+    })
+  }
+}
 
 function buildEmbed({
   roleMembersMentions,
@@ -66,199 +255,4 @@ function buildEmbed({
   }
 
   return embed
-}
-
-@SlashGroup(groupName, rootGroupName)
-@SlashGroup({
-  description: 'Управление участниками тикета',
-  root: rootGroupName,
-  name: groupName
-})
-@Discord()
-export class TicketMemberCommand {
-  private readonly ticketService = new TicketService()
-
-  @Slash({
-    description: 'Добавить участника или роль в тикет',
-    name: 'add'
-  })
-  public async add(
-    @SlashOption({
-      type: ApplicationCommandOptionType.Mentionable,
-      description: 'Кого добавить',
-      name: 'target',
-      required: true
-    })
-    target: GuildMember | Role,
-    @SlashOption({
-      description: 'Выдать права на просмотр канала-категории',
-      type: ApplicationCommandOptionType.Boolean,
-      required: false,
-      name: 'force'
-    })
-    force: boolean,
-    interaction: CommandInteraction
-  ) {
-    await interaction.deferReply({
-      ephemeral: true
-    })
-
-    const ticket = await this.ticketService.getOne({
-      channelId: interaction.channelId
-    })
-    const targetIsMember = target instanceof GuildMember
-
-    if (!ticket) {
-      return await interaction.followUp({
-        content: 'Используйте эту команду в канале, который является тикетом'
-      })
-    }
-
-    const channel = interaction.channel as ThreadChannel
-    const categoryChannel = channel.parent as TextChannel
-
-    if (
-      targetIsMember &&
-      (await channel.members.fetch(target.id).catch(() => null))
-    ) {
-      return await interaction.followUp({
-        content: `${target} уже есть в тикете`
-      })
-    }
-
-    if (
-      !categoryChannel
-        .permissionsFor(target)
-        .has(PermissionFlagsBits.ViewChannel)
-    ) {
-      if (!force) {
-        return await interaction.followUp({
-          content: `У ${target} нет прав на просмотр канала-категории ${categoryChannel}`
-        })
-      }
-      categoryChannel.permissionOverwrites.create(target, {
-        ViewChannel: true
-      })
-    }
-
-    let roleMembers: Collection<string, GuildMember> = new Collection()
-
-    if (!targetIsMember) {
-      roleMembers = (await interaction.guild!.members.fetch()).filter(
-        (m) => !m.user.bot && m.roles.resolve(target.id)
-      )
-
-      if (!roleMembers.size) {
-        return await interaction.followUp({
-          content: `На сервере нет участников с ролью ${target} (боты не учитываются)`
-        })
-      }
-
-      await channel.members.fetch()
-      roleMembers = roleMembers.filter((m) => !channel.members.resolve(m.id))
-
-      if (!roleMembers.size) {
-        return await interaction.followUp({
-          content: `Все участники с ролью ${target} уже есть в тикете`
-        })
-      }
-    }
-
-    // mentions work as members.add, but don't trigger non-deletable system message
-    channel
-      .send(
-        targetIsMember
-          ? userMention(target.id)
-          : roleMembers.map((m) => userMention(m.id)).join('')
-      )
-      .then((message) => {
-        message.delete() // we don't want to duplicate mentions of users
-        interaction.deleteReply()
-        channel.send({
-          embeds: [
-            buildEmbed({
-              roleMembersMentions: roleMembers.map((m) => userMention(m.id)),
-              moderator: interaction.user,
-              target
-            })
-          ]
-        })
-      })
-  }
-
-  @Slash({
-    description: 'Удалить участника или роль из тикета',
-    name: 'remove'
-  })
-  public async remove(
-    @SlashOption({
-      type: ApplicationCommandOptionType.Mentionable,
-      description: 'Кого удалить',
-      required: true,
-      name: 'target'
-    })
-    target: GuildMember | Role,
-    interaction: CommandInteraction
-  ) {
-    await interaction.deferReply({
-      ephemeral: true
-    })
-
-    const ticket = await this.ticketService.getOne({
-      channelId: interaction.channelId
-    })
-    const targetIsMember = target instanceof GuildMember
-
-    if (!ticket) {
-      return await interaction.followUp({
-        content: 'Используйте эту команду в канале, который является тикетом'
-      })
-    }
-
-    const channel = interaction.channel as ThreadChannel
-    // possible feature: delete categoryChannel permission overwrites if
-    // a) there were any b) it was the last user's ticket in the category
-
-    if (
-      targetIsMember &&
-      !(await channel.members.fetch(target.id).catch(() => null))
-    ) {
-      return await interaction.followUp({
-        content: `${target} нет в тикете`
-      })
-    }
-
-    let roleMembers: GuildMember[] = []
-
-    if (!targetIsMember) {
-      roleMembers = (await channel.members.fetch())
-        .map((m) => m.guildMember as GuildMember)
-        .filter((m) => !m.user.bot && m.roles.resolve(target.id))
-
-      if (!roleMembers.length) {
-        return await interaction.followUp({
-          content: `В тикете не было участников с ролью ${target} (боты не учитываются)`
-        })
-      }
-    }
-
-    if (targetIsMember) {
-      await channel.members.remove(target.id) // also creates non-deletable system message
-    } else {
-      for (const member of roleMembers.values()) {
-        await channel.members.remove(member.id) // also creates non-deletable system message
-      }
-    }
-    interaction.deleteReply()
-    channel.send({
-      embeds: [
-        buildEmbed({
-          roleMembersMentions: roleMembers.map((m) => userMention(m.id)),
-          moderator: interaction.user,
-          add: false,
-          target
-        })
-      ]
-    })
-  }
 }
